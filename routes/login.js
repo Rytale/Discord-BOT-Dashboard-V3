87895059a95bb474050e9032edb6c0bbebb6685e
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2022-10-14 20:38:11 +0200

    Add files via upload

---
+++ routes/login.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const discord = require('../bot')
+const { ensureAuthenticated, forwardAuthenticated } = require('../auth/auth');
+const passport = require('passport');
+const passtheport = require('../auth/passport');
+
+router.get('/', forwardAuthenticated, (req, res) => {
+    res.render('login/login',{
+        user:discord.client.user.username,
+        avatar:discord.client.user.avatarURL()
+    })
+})
+
+router.get('/api', forwardAuthenticated,(req,res, next)=>{
+    passport.authenticate('discord', {
+        successRedirect: '/home',
+        failureRedirect: '/login',
+        failureFlash: true
+      })(req, res, next);
+})
+
+module.exports = router;
